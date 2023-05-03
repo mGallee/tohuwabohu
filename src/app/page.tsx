85218637d055a72bf0f8e5ba2772459bab249387
Matthias Gallee <mGallee@users.noreply.github.com>
2023-05-02 20:08:45 -0600

    style: add more spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <section className="flex flex-1 flex-col justify-center items-center">
-      <h1 className="text-6xl mb-6 animate-glow">Tohuwabohu</h1>
+    <section className="flex flex-1 flex-col justify-center items-center gap-8">
+      <h1 className="text-6xl animate-glow mt-4 mb-6">Tohuwabohu</h1>
       <div className="text-4xl">Colorful rave experiences in Vienna</div>
     </section>
   );
