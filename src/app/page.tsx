4553db3c842b0c86940e432cd8857f296fa69c2c
Matthias Gallee <gallee.matthias@gmail.com>
2024-12-02 17:44:49 +0100

    feat: improve seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <section className="flex flex-1 flex-col items-center justify-center gap-8">
-      <h1 className="mb-6 mt-4 animate-glow text-6xl">Tohuwabohu</h1>
-      <div className="text-4xl">Colorful rave experiences in Vienna</div>
+    <section className="flex flex-1 flex-col items-center justify-center gap-4 text-center md:gap-8">
+      <h1 className="my-6 animate-glow text-6xl md:text-8xl">Tohuwabohu</h1>
+      <p className="text-2xl md:text-4xl">
+        We offer safe and colorful rave experiences in Vienna
+      </p>
     </section>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,11 +19,11 @@ const socialMediaItems = [
 
 export default function Footer() {
   return (
-    <footer className="relative flex flex-row flex-wrap items-center justify-center gap-6 p-8 text-2xl">
+    <footer className="relative flex flex-row flex-wrap items-center justify-center gap-3 p-4 text-center md:gap-6 md:p-8">
       {socialMediaItems.map(({ label, href }) => (
         <a
           key={label}
-          className="select-none outline-0 hover:animate-glow active:animate-glow"
+          className="select-none text-xl outline-0 hover:animate-glow active:animate-glow md:text-2xl"
           href={href}
           target="_blank">
           {label}
