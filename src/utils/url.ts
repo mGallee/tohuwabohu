5e13c8688a0198b6774887381fcb8c5c1df0e233
Matthias Gallee <gallee.matthias@gmail.com>
2024-12-03 14:31:55 +0100

    fix: base url

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,5 @@
 export const protocol =
   process.env.NODE_ENV === 'production' ? 'https' : 'http';
-export const domain = process.env.VERCEL_URL || 'localhost:3000';
+export const domain =
+  process.env.VERCEL_PROJECT_PRODUCTION_URL || 'localhost:3000';
 export const baseUrl = `${protocol}://${domain}`;
