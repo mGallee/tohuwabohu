c274d509f977a0a38c72fd98325ce2879c7c2bac
Matthias Gallee <gallee.matthias@gmail.com>
2025-03-04 22:08:34 +0100

    chore: update description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <section className="flex flex-1 flex-col items-center justify-center gap-8 text-center md:gap-16">
       <h1 className="my-4 animate-glow text-6xl md:text-8xl">Tohuwabohu</h1>
       <p className="text-2xl md:text-4xl">
-        We offer safe and colorful rave experiences in Vienna
+        Safe*r space thanks to our awareness team
       </p>
       <a
         className="rounded-xl border-2 border-stone-50 px-4 py-2 pb-4 text-2xl leading-none transition-all hover:animate-glow hover:bg-stone-50 hover:text-black active:animate-glow active:bg-stone-50 active:text-black md:text-4xl md:leading-none"
