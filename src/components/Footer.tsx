a23a1b03563c436eb64338b6a132be1a1b6de2b9
Matthias Gallee <gallee.matthias@gmail.com>
2025-10-13 10:19:44 +0200

    chore: update dependencies and add button component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,11 @@
-const socialMediaItems = [
-  {
-    label: 'Instagram',
-    href: 'https://www.instagram.com/tohuwabohu.vienna',
-  },
-  {
-    label: 'Facebook',
-    href: 'https://www.facebook.com/tohuwabohu.vienna',
-  },
-  {
-    label: 'Soundcloud',
-    href: 'https://soundcloud.com/tohuwabohu-vienna',
-  },
-  {
-    label: 'Telegram',
-    href: 'https://t.me/TohuwabohuVienna',
-  },
-];
+import { SOCIAL_MEDIA_ITEMS } from '@/constants/social-media';
 
 export default function Footer() {
   return (
     <footer className="relative flex flex-row flex-wrap items-center justify-center gap-3 p-4 text-center md:gap-6 md:p-8">
-      {socialMediaItems.map(({ label, href }) => (
+      {SOCIAL_MEDIA_ITEMS.map(({ label, href }) => (
         <a
-          key={label}
+          key={`${label}_${href}`}
           className="hover:animate-glow active:animate-glow text-xl outline-0 transition-all md:text-2xl"
           href={href}
           target="_blank">
