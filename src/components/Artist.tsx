0e1647a85ce9e675f835014669917f395589f044
Matthias Gallee <matthias@social.quest>
2025-11-09 21:34:34 +0100

    chore: improve sound cloud player load performance

--- src/components/Artist.tsx
+++ src/components/Artist.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { ARTIST_MAP, ArtistKey } from '@/constants/artist';
 import Link from '@/components/Link';
+import SoundCloudPlayer from '@/components/SoundCloudPlayer';
 
 interface ArtistProps {
   artist: ArtistKey;
@@ -16,26 +17,20 @@ export default function Artist({ artist }: ArtistProps) {
           className="flex flex-row items-center gap-2"
           href={`https://soundcloud.com/${soundCloud.username}`}
           target="_blank">
-          <div>
-            <Image
-              className="aspect-square h-[40] w-[40] rounded-full md:h-[50] md:w-[50]"
-              src={`/images/artists/${profilePicture}`}
-              width="50"
-              height="50"
-              alt={name}
-            />
-          </div>
-          <div>
-            <h4 className="mb-1 text-2xl md:mb-2 md:text-4xl">{name}</h4>
-          </div>
+          <Image
+            className="aspect-square h-[40] w-[40] rounded-full md:h-[50] md:w-[50]"
+            src={`/images/artists/${profilePicture}`}
+            width="50"
+            height="50"
+            alt={`Profile picture of ${name}`}
+          />
+          <span className="mb-1 text-2xl md:mb-2 md:text-4xl">{name}</span>
         </Link>
       </div>
-      <iframe
-        className="rounded-xl border-0"
-        width="100%"
-        height="180"
-        allow="autoplay; encrypted-media *"
-        src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A${soundCloud.trackId}&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=true&visual=true`}></iframe>
+      <SoundCloudPlayer
+        url={`https://api.soundcloud.com/tracks/soundcloud%3Atracks%3A${soundCloud.trackId}`}
+        title={`Soundcloud: ${name}`}
+      />
     </div>
   );
 }
