a5fc152811353ddf1de834bf49e0d58c3b48da3b
Matthias Gallee <matthias@social.quest>
2025-11-10 23:14:56 +0100

    chore: extend footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,21 @@ import Link from '@/components/Link';
 
 export default function Footer() {
   return (
-    <footer className="flex flex-row flex-wrap items-center justify-center gap-3 p-4 text-center md:gap-6 md:p-8">
-      {SOCIAL_MEDIA_ITEMS.map(({ label, href }) => (
-        <Link
-          key={`${label}_${href}`}
-          className="hover:animate-glow active:animate-glow text-xl outline-0 transition-all md:text-2xl"
-          href={href}
-          target="_blank">
-          {label}
-        </Link>
-      ))}
+    <footer className="flex flex-col items-center gap-3 p-4 md:gap-6 md:p-8">
+      <div className="flex flex-row flex-wrap items-center justify-center gap-3 text-center md:gap-6">
+        {SOCIAL_MEDIA_ITEMS.map(({ label, href }) => (
+          <Link
+            key={`${label}_${href}`}
+            className="hover:animate-glow active:animate-glow text-xl outline-0 transition-all md:text-2xl"
+            href={href}
+            target="_blank">
+            {label}
+          </Link>
+        ))}
+      </div>
+      <div className="text-center text-sm text-stone-300">
+        Tohuwabohu Kultur und Musikverein
+      </div>
     </footer>
   );
 }
