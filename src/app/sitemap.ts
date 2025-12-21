19c34cf87e70ef6ab23901867bde9fe919ea1a98
Matthias Gallee <matthias@social.quest>
2025-12-21 20:55:38 +0100

    chore: update sitemap priorities

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,33 +6,33 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseUrl,
-      priority: 0.5,
+      priority: 1,
       changeFrequency: 'weekly',
     },
     {
       url: `${baseUrl}/events`,
-      priority: 0.5,
+      priority: 0.8,
       changeFrequency: 'weekly',
     },
     {
       url: `${baseUrl}/artists`,
-      priority: 0.5,
+      priority: 0.8,
       changeFrequency: 'weekly',
     },
     ...ARTISTS_DATA.map((artist) => ({
       url: `${baseUrl}/artists/${artist.slug}`,
-      priority: 0.5,
+      priority: 0.6,
       changeFrequency: 'weekly' as const,
     })),
     {
       url: `${baseUrl}/awareness`,
       priority: 0.5,
-      changeFrequency: 'weekly',
+      changeFrequency: 'monthly',
     },
     {
       url: `${baseUrl}/imprint`,
-      priority: 0.5,
-      changeFrequency: 'weekly',
+      priority: 0.3,
+      changeFrequency: 'monthly',
     },
   ];
 }
