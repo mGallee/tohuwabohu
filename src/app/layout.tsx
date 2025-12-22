3877095c4fd3d6d3c66f37dd7a4015165dc6c416
Matthias Gallee <matthias@social.quest>
2025-12-22 09:46:01 +0100

    chore: add vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import localFont from 'next/font/local';
 import { Roboto } from 'next/font/google';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 import { ReactNode } from 'react';
 import { Metadata, Viewport } from 'next';
 import { baseUrl } from '@/utils/url';
@@ -30,7 +31,8 @@ const robotoFont = Roboto({
 });
 
 const title = 'Tohuwabohu | Kultur- und Musikverein';
-const description = 'Safe*r space thanks to our awareness team!';
+const description =
+  'Tohuwabohu is a collective in Vienna that loves music, art, and bringing people together. We throw events that focus on good sound, nice visuals, and an open, respectful atmosphere where everyone should feel welcome.';
 
 export const metadata: Metadata = {
   title: {
@@ -106,6 +108,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         </main>
         <Footer />
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
