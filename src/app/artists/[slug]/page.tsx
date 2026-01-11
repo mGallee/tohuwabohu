90ab0764f612641050374c508d7ffe4c3d400e36
Matthias Gallee <gallee.matthias@gmail.com>
2026-01-11 17:54:42 +0100

    chore: add instagram resident advisor

--- src/app/artists/[slug]/page.tsx
+++ src/app/artists/[slug]/page.tsx
@@ -2,11 +2,11 @@ import SoundCloudPlayer from '@/components/SoundCloudPlayer';
 import Image from 'next/image';
 import { ARTISTS_DATA } from '@/constants/artist';
 import { notFound } from 'next/navigation';
-import Button from '@/components/Button';
 import { Metadata, ResolvingMetadata } from 'next';
 import { generateArtistJsonLd } from '@/utils/jsonLd';
 import JsonLd from '@/components/JsonLd';
 import { getArtistSlug } from '@/utils/helper';
+import Link from '@/components/Link';
 
 interface ArtistPageProps {
   params: Promise<{ slug: string }>;
@@ -61,7 +61,7 @@ export default async function ArtistPage({ params }: ArtistPageProps) {
     <section className="flex flex-col items-center gap-8 md:gap-16">
       <JsonLd data={generateArtistJsonLd(artist)} />
       <Image
-        className="aspect-square h-[220] w-[220] rounded-full border-2 border-stone-50 md:h-[300] md:w-[300]"
+        className="aspect-square h-[220] w-[220] rounded-full border-2 border-stone-50 md:h-[320] md:w-[320]"
         src={artist.profilePicture}
         alt={`Profile picture of ${artist.name}`}
         draggable={false}
@@ -72,18 +72,30 @@ export default async function ArtistPage({ params }: ArtistPageProps) {
       <p className="text-center text-2xl text-balance whitespace-pre-wrap md:text-4xl">
         {artist.description}
       </p>
-      <div className="flex flex-col items-center gap-2 self-stretch md:gap-4">
-        <SoundCloudPlayer
-          url={`https://api.soundcloud.com/tracks/soundcloud:tracks:${artist.soundCloud.trackId}`}
-          title={`Soundcloud: ${artist.name}`}
-        />
-        <Button
-          as="a"
+      <SoundCloudPlayer
+        url={`https://api.soundcloud.com/tracks/soundcloud:tracks:${artist.soundCloud.trackId}`}
+        title={`Soundcloud: ${artist.name}`}
+      />
+      <div className="flex flex-col items-center gap-2 text-center text-xl md:flex-row md:justify-evenly md:gap-4 md:self-stretch md:text-2xl">
+        <Link
           href={`https://soundcloud.com/${artist.soundCloud.username}`}
-          target="_blank"
-          variant="text">
-          Visit SoundCloud
-        </Button>
+          target="_blank">
+          SoundCloud
+        </Link>
+        {artist.instagram ? (
+          <Link
+            href={`https://www.instagram.com/${artist.instagram.username}`}
+            target="_blank">
+            Instagram
+          </Link>
+        ) : null}
+        {artist.residentAdvisor ? (
+          <Link
+            href={`https://ra.co/dj/${artist.residentAdvisor.username}`}
+            target="_blank">
+            Resident Advisor
+          </Link>
+        ) : null}
       </div>
     </section>
   );
