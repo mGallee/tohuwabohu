c04c3e3d2c62a8c0261b09e4b50a018019e4aacd
Matthias Gallee <gallee.matthias@gmail.com>
2026-02-09 21:12:52 +0100

    fix: get base URL

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,12 +3,15 @@ export function getBaseUrl() {
     return window.location.origin;
   }
 
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+  if (
+    process.env.VERCEL_ENV === 'production' &&
+    process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
 
-  if (process.env.NEXT_PUBLIC_SITE_URL) {
-    return process.env.NEXT_PUBLIC_SITE_URL;
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
   }
 
   return `http://localhost:${process.env.PORT || 3000}`;
